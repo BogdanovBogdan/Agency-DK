783fb9c73a21054d53e99c177401629d6bbdc361
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-04-30 17:09:53 +0300

    minor fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,57 @@
 window.addEventListener("DOMContentLoaded", () => {
 	"use strict";
 
+	const calcScroll = () => {
+		let calcBox = document.createElement("div");
+		calcBox.style.width = "50px";
+		calcBox.style.height = "50px";
+		calcBox.style.overflowY = "scroll";
+		calcBox.style.visibility = "hidden";
+		document.body.appendChild(calcBox);
+		let scrollWidth = calcBox.offsetWidth - calcBox.clientWidth;
+		calcBox.remove();
+
+		return scrollWidth;
+	};
+
+
+
+	// modals
+	const bindModals = (triggerSelectors, modalSelector, closeSelector) => {
+		const triggers = document.querySelectorAll(triggerSelectors),
+			modal = document.querySelector(modalSelector),
+			close = document.querySelector(closeSelector),
+			scrollWidth = calcScroll();
+
+		triggers.forEach(item => {
+			item.addEventListener("click", (e) => {
+				if (e.target) {
+					e.preventDefault();
+				}
+				modal.style.display = "block";
+				document.querySelector("html").style.overflow = "hidden";
+				document.querySelector("html").style.marginRight = `${scrollWidth}px`;
+			});
+		});
+
+		close.addEventListener("click", () => {
+			modal.style.display = "none";
+			document.querySelector("html").style.overflow = "";
+			document.querySelector("html").style.marginRight = `0px`;
+		});
+
+		modal.addEventListener("click", (e) => {
+			if (e.target === modal) {
+				modal.style.display = "none";
+				document.querySelector("html").style.overflow = "";
+				document.querySelector("html").style.marginRight = `0px`;
+			}
+		});
+	};
+	bindModals("#callback-request", ".popup-callback", ".popup-callback .popup__close");
+
+
+
 	// smooth scrolling to anchors
 	const anchors = document.querySelectorAll('a[href*="#"]'),
 		nav = document.querySelector(".nav-container");
@@ -30,63 +81,89 @@ window.addEventListener("DOMContentLoaded", () => {
 	};
 
 
-	const calcScroll = () => {
-		let calcBox = document.createElement("div");
-		calcBox.style.width = "50px";
-		calcBox.style.height = "50px";
-		calcBox.style.overflowY = "scroll";
-		calcBox.style.visibility = "hidden";
-		document.body.appendChild(calcBox);
-		let scrollWidth = calcBox.offsetWidth - calcBox.clientWidth;
-		calcBox.remove();
-
-		return scrollWidth;
-	};
-
-
-	const bindModals = (triggerSelectors, modalSelector, closeSelector) => {
-		const triggers = document.querySelectorAll(triggerSelectors),
-			modal = document.querySelector(modalSelector),
-			close = document.querySelector(closeSelector),
-			scrollWidth = calcScroll();
-
-		triggers.forEach(item => {
-			item.addEventListener("click", (e) => {
-				if (e.target) {
-					e.preventDefault();
-				}
-				modal.style.display = "block";
-				document.body.style.overflow = "hidden";
-				document.body.style.marginRight = `${scrollWidth}px`;
-			});
-		});
-
-		close.addEventListener("click", () => {
-			modal.style.display = "none";
-			document.body.style.overflow = "";
-			document.body.style.marginRight = `0px`;
-		});
-
-		modal.addEventListener("click", (e) => {
-			if (e.target === modal) {
-				modal.style.display = "none";
-				document.body.style.overflow = "";
-				document.body.style.marginRight = `0px`;
-			}
-		});
-	};
-	bindModals("#callback-request", ".popup-callback", ".popup-callback .popup__close");
-
-
 
 	// animation burger menu
-	const triggerMenu = document.querySelector('.trigger'),
-		navigation = document.querySelector('.nav-container');
+	if (window.innerWidth < 768) {
+		const triggerMenu = document.querySelector('.trigger'),
+			navigation = document.querySelector('.nav-container'),
+			navContainer = document.querySelector('.nav-menu-container');
+	
+		navContainer.style.zIndex = "-10"
+	
+		triggerMenu.addEventListener("click", () => {
+			navigation.classList.toggle('animated');
+			document.querySelector("html").style.overflow == "hidden" ? document.querySelector("html").style.overflow = "" : document.querySelector("html").style.overflow = "hidden";
+			
 
-	triggerMenu.addEventListener("click", () => {
-		navigation.classList.toggle('animated');
-		document.querySelector("html").style.overflow == "hidden" ? document.querySelector("html").style.overflow = "" : document.querySelector("html").style.overflow = "hidden";
-	});
+			if (!navigation.classList.contains('animated')) {
+				setTimeout(() => {
+					navContainer.style.zIndex = "-10";
+				}, 1000);
+			} else {
+				navContainer.style.zIndex = "2";
+			}
+		});
+	}
 
 
+
+	// post data form
+	const forms = () => {
+		const forms = document.querySelectorAll("form"),
+			inputs = document.querySelectorAll("input");
+
+		const message = {
+			loading: 'Загрузка...',
+			success: 'Спасибо! Скоро мы с вами свяжемся',
+			failure: 'Что-то пошло не так...'
+		};
+
+		const clearInputs = () => {
+			inputs.forEach(item => {
+				item.value = '';
+				item.checked = false;
+			})
+		};
+
+		const postData = async (url, data) => {
+			document.querySelector('.status').textContent = message.loading;
+			let res = await fetch(url, {
+				method: "post",
+				body: data
+			});
+			return await res.text();
+		};
+
+		forms.forEach(item => {
+			item.addEventListener('submit', (e) => {
+				e.preventDefault()
+
+				let statusMessage = document.createElement('div');
+				statusMessage.classList.add('status');
+				item.appendChild(statusMessage);
+
+				const formData = new FormData(item);
+
+				postData('assets/server.php', formData)
+					.then(res => {
+						console.log('success: ', res);
+						statusMessage.textContent = message.success;
+					})
+					.catch(() => statusMessage.textContent = message.failure)
+					.finally(() => {
+						clearInputs();
+						setTimeout(() => {
+							statusMessage.remove();
+						}, 4000);
+
+					});
+			});
+
+		});
+
+
+	};
+	forms();
+
+	
 });
